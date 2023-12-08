60db00a62ac07ec085d3882ce97e7255887a1bf3
Dinesh <dhanjeehartik@gmail.com>
2023-12-08 14:55:25 +0500

    Lil changes.

--- Augmenting FLow.py
+++ Augmenting FLow.py
@@ -1,4 +1,43 @@
-import time
+class Graph:
+    def __init__(self, graph):
+        self.graph = graph
+        self.row = len(graph)
+        
+    def BFS(self, s, t, parent):
+        # Mark all the vertices as not visited
+        visited = [False] * (self.row)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        # Standard BFS Loop
+        while queue:
+            # Dequeue a vertex from queue and print it
+            u = queue.pop(0)
+
+            # Get all adjacent vertices of the dequeued vertex u
+            # If a adjacent has not been visited, then mark it
+            # visited and enqueue it
+            for ind, val in enumerate(self.graph[u]):
+                if visited[ind] == False and val > 0:
+                    # If we find a connection to the sink node,
+                    # then there is no point in BFS anymore
+                    # We just have to set its parent and can return true
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+                    if ind == t:
+                        return True
+
+        # We didn't reach sink in BFS starting
+        # from source, so return false
+        return False
+
+
 
 
 # Helping functions
@@ -32,91 +71,49 @@ def convert_to_dict_matrix(g):
     # Return the resulting adjacency matrix g2
     return dict_matrix
 
+# Returns the maximum flow from s to t in the given graph
+def FordFulkerson(graph, source, sink):
+    # This array is filled by BFS and to store path
+    parent = [-1] * (graph.row)
 
-# This class represents a directed graph
-# using adjacency matrix representation
-class Graph:
-    def __init__(self, graph):
-        self.graph = graph  # residual graph
-        self.ROW = len(graph)
-        # self.COL = len(gr[0])
+    max_flow = 0  # There is no flow initially
 
-    """Returns true if there is a path from source 's' to sink 't' in
-    residual graph. Also fills parent[] to store the path """
+    # Augment the flow while there is path from source to sink
+    while graph.BFS(source, sink, parent):
+        # Find minimum residual capacity of the edges along the
+        # path filled by BFS. Or we can say find the maximum flow
+        # through the path found.
+        path_flow = float("Inf")
+        s = sink
+        while s != source:
+            path_flow = min(path_flow, graph.graph[parent[s]][s])
+            s = parent[s]
 
-    def BFS(self, s, t, parent):
-        # Mark all the vertices as not visited
-        visited = [False] * (self.ROW)
+        # Add path flow to overall flow
+        max_flow += path_flow
 
-        # Create a queue for BFS
-        queue = []
+        # update residual capacities of the edges and reverse edges
+        # along the path
+        v = sink
+        while v != source:
+            u = parent[v]
+            graph.graph[u][v] -= path_flow
+            graph.graph[v][u] += path_flow
+            v = parent[v]
 
-        # Mark the source node as visited and enqueue it
-        queue.append(s)
-        visited[s] = True
+        temp_graph = convert_to_dict_matrix(graph.graph)
+    # Draw the final graph
+    # DiGraph(temp_graph).show(edge_labels=True)
 
-        # Standard BFS Loop
-        while queue:
-            # Dequeue a vertex from queue and print it
-            u = queue.pop(0)
-
-            # Get all adjacent vertices of the dequeued vertex u
-            # If a adjacent has not been visited, then mark it
-            # visited and enqueue it
-            for ind, val in enumerate(self.graph[u]):
-                if visited[ind] == False and val > 0:
-                    # If we find a connection to the sink node,
-                    # then there is no point in BFS anymore
-                    # We just have to set its parent and can return true
-                    queue.append(ind)
-                    visited[ind] = True
-                    parent[ind] = u
-                    if ind == t:
-                        return True
-
-        # We didn't reach sink in BFS starting
-        # from source, so return false
-        return False
-
-    # Returns the maximum flow from s to t in the given graph
-    def FordFulkerson(self, source, sink):
-        # This array is filled by BFS and to store path
-        parent = [-1] * (self.ROW)
-
-        max_flow = 0  # There is no flow initially
-
-        # Augment the flow while there is path from source to sink
-        while self.BFS(source, sink, parent):
-            # Find minimum residual capacity of the edges along the
-            # path filled by BFS. Or we can say find the maximum flow
-            # through the path found.
-            path_flow = float("Inf")
-            s = sink
-            while s != source:
-                path_flow = min(path_flow, self.graph[parent[s]][s])
-                s = parent[s]
-
-            # Add path flow to overall flow
-            max_flow += path_flow
-
-            # update residual capacities of the edges and reverse edges
-            # along the path
-            v = sink
-            while v != source:
-                u = parent[v]
-                self.graph[u][v] -= path_flow
-                self.graph[v][u] += path_flow
-                v = parent[v]
-
-            temp_graph = convert_to_dict_matrix(self.graph)
-        # Draw the final graph
-        DiGraph(temp_graph).show(edge_labels=True)
-
-        return max_flow
+    return max_flow
 
 
 # This means just main function
 if __name__ == "__main__":
+    """
+        Hamza take the input in the given form of dictionary
+        or in the form of g2(list). You can convert them.
+    """
     # Need to take the input in this form.
     g1 = {
         0: {1: 16, 2: 13},
@@ -142,5 +139,5 @@ if __name__ == "__main__":
     sink = 5
 
     g = Graph(convert_to_incident_matrix(g1))
-    max_flow = g.FordFulkerson(source, sink)
+    max_flow = FordFulkerson(g, source, sink)
     print(f"The maximum possible flow is {max_flow}")
